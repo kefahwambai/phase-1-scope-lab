53d6628529e0230578ef2e73e15b64662676bfe4
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-27 23:42:34 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+
+let  upperCaseCustomerName = () => customerName.toUpperCase()
+
+
+let  setBestCustomer = () => bestCustomer = "not bob"; 
+
+let overwriteBestCustomer = () => bestCustomer= 'maybe bob';
+
+const leastFavoriteCustomer = () =>leastFavoriteCustomer = "bob's sister";
+
+let  changeLeastFavoriteCustomer = () => leastFavoriteCustomer = "bob's brother";
+
+
+
